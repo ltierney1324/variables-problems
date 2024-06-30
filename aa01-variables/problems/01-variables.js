b22daaa80020cb9e22a096089a6ea32a5828704f
Your Name <Your Email>
2024-06-30 10:51:06 -0400

    finished

--- aa01-variables/problems/01-variables.js
+++ aa01-variables/problems/01-variables.js
@@ -6,35 +6,41 @@ test your work by running your code and the test specs!
 // 1. Declare a variable called `firstName` and assign it your first name
 //    as a string:
 
-// Your code here 
-
+// Your code here
+let firstName = 'Lexie';
 
 // 2. Declare a variable called `lastName` and assign it your last name
 //    as a string:
 
-// Your code here 
-
+// Your code here
+let lastName = 'Tierney';
 
 // 3. Declare a variable called `age`:
 
-// Your code here 
+// Your code here
+let age;
 
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+// Your code here
+console.log(firstName,lastName,age);
 
 
 // 5. Assign the `age` variable to a number:
 
-// Your code here 
+// Your code here
+age =30;
 
 
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+// Your code here
+console.log(firstName, lastName, age);
+
+
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
